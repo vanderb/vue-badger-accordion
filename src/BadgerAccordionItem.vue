d053a7c936142cb7a9cc99623178f781250a9f12
Freerk van Zeijl <freerk.vanzeijl@ultrabold.com>
2019-07-02 11:10:12 +0200

    Removed wrong implemented library from badger-accordion-item.

--- src/BadgerAccordionItem.vue
+++ src/BadgerAccordionItem.vue
@@ -20,15 +20,6 @@
 </template>
 
 <script>
-    import Vue from 'vue'
-    import { library } from '@fortawesome/fontawesome-svg-core'
-    import { fas } from '@fortawesome/free-solid-svg-icons'
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-    library.add(fas);
-
-    Vue.component('font-awesome-icon', FontAwesomeIcon)
-
     export default {
         name: 'BadgerAccordionItem',
         mounted() {
