dd88eeb0d983ffd90cef5da174ebede0181a021c
Franz Thüs <franz.thues@hhey.studio>
2021-06-18 17:06:49 +0200

    Add .vue extensions to component imports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import ImportBadgerAccordion from './src/BadgerAccordion'
-import ImportBadgerAccordionItem from './src/BadgerAccordionItem'
+import ImportBadgerAccordion from './src/BadgerAccordion.vue'
+import ImportBadgerAccordionItem from './src/BadgerAccordionItem.vue'
 
 export const BadgerAccordion = ImportBadgerAccordion;
 export const BadgerAccordionItem = ImportBadgerAccordionItem;
